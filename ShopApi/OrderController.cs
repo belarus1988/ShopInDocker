286b4b58406c13654c29e7c11906e7dbd2dc733b
Dzmitry Kamarou <dzmitry_kamarou1@epam.com>
2021-03-07 20:05:08 +0400

    Add ShopApi app

---
+++ ShopApi/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ShopApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly HttpClient _client = new HttpClient();
+
+        public OrderController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create()
+        {
+            var id = Guid.NewGuid().ToString();
+
+            // some logic...
+
+            // send notification to customer
+            var stringPayload = JsonConvert.SerializeObject(new { orderId = id });
+            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+            var notifierUrl = _config.GetValue<string>("NotifierUrl");
+            var msg = await _client.PostAsync(notifierUrl, httpContent);
+            if (msg.StatusCode != System.Net.HttpStatusCode.OK)
+                return BadRequest("Can't call Notifier");
+
+            return new JsonResult(id);
+        }
+    }
+}
